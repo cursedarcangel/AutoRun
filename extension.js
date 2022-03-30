7c9df7215968ab30739e519819eefd5e79f1fe43
cursedarcangel <ufdoisa@gmail.com>
2022-03-30 18:09:53 -0400

    Able to specify custom command and run it

--- extension.js
+++ extension.js
@@ -5,14 +5,30 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	console.log('Congratulations, your extension "autorun" is now active!');
-
 	let compileandrun = vscode.commands.registerCommand('autorun.compileandrun', function () {
-		vscode.window.showInformationMessage("Compile and Run");
+		let cmd = vscode.workspace.getConfiguration().inspect('autorun.config').workspaceValue;
+		if (vscode.window.terminals.length == 0) {
+			vscode.window.createTerminal("Terminal");
+			vscode.window.terminals[0].show();
+		} else {
+			vscode.window.terminals[0].show();
+		}
+		vscode.window.terminals[0].sendText(cmd);
 	});
 
 	let configure = vscode.commands.registerCommand('autorun.configure', function () {
-		vscode.window.showInformationMessage('Start configuring');
+		if (vscode.workspace.workspaceFolders[0] == null) {
+			vscode.window.showErrorMessage("Please open a project folder");
+		} else {
+			async function getConfig() {
+				const cmd = await vscode.window.showInputBox({
+					title: "What is the command to compile and run?",
+					placeHolder: "./a.out"
+				});
+				vscode.workspace.getConfiguration().update('autorun.config', cmd);
+			}
+			getConfig();
+		}
 	});
 	context.subscriptions.push(compileandrun, configure);
 }
